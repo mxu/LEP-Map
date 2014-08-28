b304ef8042149e0c512a82543a727a1410164c83
Michael Xu <mxu11290@yahoo.com>
2014-08-27 21:43:30 -0400

    content changes, squished nav bar for tablet, enabled state select

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -43,7 +43,7 @@ angular.module('lepMapApp')
       };
 
       $scope.tooltips = {
-        'representative': 'The LES Scorecard details the 15 indicators used to construct the member\'s Legislative Effectiveness Score, based on sponsored bill types and their progression through the legislative process. (See Methodology for more details)',
+        'representative': 'Click on a Representative\'s name to see her LES Scorecard. The LES Scorecard details the 15 indicators used to construct the member\'s Legislative Effectiveness Score, based on sponsored bill types and their progression through the legislative process. (See Methodology for more details.)',
         'partyRank': 'The Rank in Party is the numerical ranking of a member of the House of Representative’s Legislative Effectiveness Score in comparison to other members of her political party in a given two-year Congress, where “1” connotes the highest Legislative Effectiveness Score.',
         'benchmark': 'The Benchmark Score for a member of the House of Representatives is the expected Legislative Effectiveness Score for a Representative in a given Congress who is of the same political party, has served the same number of terms in Congress, and has held the same number of Committee and/or Subcommittee Chairmanships as the given member.',
         'les': 'The Legislative Effectiveness Score (LES) is a summary measure that captures how successful each member of the House of Representatives is, in a given two-year Congress, at moving her agenda items, which are coded for their relative substantive significance, through the different steps of the legislative process, which begins with introducing a bill into the House until it (possibly) becomes law. The average LES is 1.0',
@@ -60,6 +60,8 @@ angular.module('lepMapApp')
         'law': 'Number of bills becoming law',
         'zip': 'Search the most recent congress within the dataset by zip'
       };
+
+      $scope.stateNames = [];
 
       $scope.setOrder = function(field) {
         if($scope.checkOrder(field)) {
@@ -168,8 +170,15 @@ angular.module('lepMapApp')
               'cAbc': row.c_abc
             };
 
+            if($scope.stateNames.indexOf(newRep.stName) === -1) {
+              $scope.stateNames.push(newRep.stName);
+            }
+
             $scope.congressData[row.congress].states[row.st_name].push(newRep);
           }
+
+          console.log('states:');
+          console.log($scope.stateNames);
 
           // select the most recent congress
           $scope.select.latest = latestCongress.toString();
@@ -224,6 +233,11 @@ angular.module('lepMapApp')
         }
         $scope.select.reps = $scope.select.congress.states[$scope.select.state];
         $document.scrollToElement(angular.element('#repList'), 50, 300);
+      };
+
+      $scope.switchState = function() {
+        console.log('switch to ' + $scope.select.state);
+        stateService.prepForBroadcast($scope.select.state, null);
       };
 
       // return glyph name based on expectation status
